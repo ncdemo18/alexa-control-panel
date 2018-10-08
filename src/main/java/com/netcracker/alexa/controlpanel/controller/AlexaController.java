31a378a72bace1085933bc93046ca543bf12c6b9
ncdemo18 <ncdemo18@gmail.com>
2018-10-08 17:14:22 +0400

    next ricky page

--- src/main/java/com/netcracker/alexa/controlpanel/controller/AlexaController.java
+++ src/main/java/com/netcracker/alexa/controlpanel/controller/AlexaController.java
@@ -1,9 +1,20 @@
 package com.netcracker.alexa.controlpanel.controller;
 
+import com.netcracker.alexa.controlpanel.model.Command;
+import com.netcracker.alexa.controlpanel.model.CommandType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AlexaController {
+
+    private final SimpMessageSendingOperations sendingOperations;
+
+    @Autowired
+    public AlexaController(SimpMessageSendingOperations sendingOperations) {
+        this.sendingOperations = sendingOperations;
+    }
 
     @GetMapping("/hello")
     String get(){
@@ -13,6 +24,9 @@ public class AlexaController {
     @PostMapping("/handle_user_request")
     @ResponseBody
     String handleRequest(@RequestParam("userMessage") String message){
+        if(message.equals("next page")) {
+            sendingOperations.convertAndSend("/topic/user/ricky", new Command(CommandType.NEXT_PAGE));
+        }
         return "User message: " + message + "!";
     }
 }
