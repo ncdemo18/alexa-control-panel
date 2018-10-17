4254c74065a05512f35dc6c59d8c1f36a96e3c94
ncdemo18 <ncdemo18@gmail.com>
2018-10-17 14:03:26 +0400

    get address from spring

--- src/main/java/com/netcracker/alexa/controlpanel/service/AddressService.java
+++ src/main/java/com/netcracker/alexa/controlpanel/service/AddressService.java
@@ -1,12 +1,18 @@
 package com.netcracker.alexa.controlpanel.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Service
 public class AddressService {
-    private final String applicationURL = "https://alexa-control-panel.herokuapp.com/";
+
+    @Autowired
+    private HttpServletRequest request;
 
     public String getFullURL(String userLogin, String url) {
+        String applicationURL = request.getRequestURL().toString();
         return applicationURL + "user/" + userLogin + "/" + url;
     }
 }
